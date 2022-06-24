07c6ae27b553d63502e5b143b638604c820c1e5d
Ryan <ryan@zipcodersmbp55.lan>
2022-06-24 09:09:03 -0400

    added first Scanner object

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,14 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  */
 public class Main {
 
-    public static void main(String[] args){
-        
+    public static void main(String[] args) {
+        String line;
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("zipcode rocks!");
     }
 }
