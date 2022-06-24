495d4d4741d7b211c7006b47ffdbc708d7c57494
Ryan <ryan@zipcodersmbp55.lan>
2022-06-24 13:06:32 -0400

    added last guess tracking

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,6 +7,7 @@ public class Main {
 
     public static void main(String[] args) {
         int numInput;
+        int lastGuess = 0; // keeps track of previous guess if user has to guess again
         int guessCount = 0;
         boolean correctGuess = false;
         int secretNum = 86;
@@ -15,17 +16,24 @@ public class Main {
             System.out.print("Guess a number between 1 and 100: ");
             Scanner in = new Scanner(System.in);
             numInput = Integer.parseInt(in.nextLine());
-            guessCount++;
 
-            if(numInput == secretNum) {
-                correctGuess = true;
-                System.out.println("You guessed correctly! It took you " + guessCount + " tries.");
-            }
-            else if(numInput < secretNum) {
-                System.out.println(numInput + " is too low. Try Again.");
+            if (guessCount == 0 || numInput != lastGuess){
+                guessCount++;
+                if(numInput == secretNum) {
+                    correctGuess = true;
+                    System.out.println("You guessed correctly! It took you " + guessCount + " tries.");
+                }
+                else if(numInput < secretNum) {
+                    System.out.println(numInput + " is too low. Try Again.");
+                }
+                else {
+                    System.out.println(numInput + " is too high. Try Again.");
+                }
+
+                lastGuess = numInput;
             }
             else {
-                System.out.println(numInput + " is too high. Try Again.");
+                System.out.println("You just guessed that, try again.");
             }
         }
     }
